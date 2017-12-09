74fe36a6c464baa637821cf9900408032502b481
鵜巷 <u@chimata.org>
2017-12-09 09:31:35 +0900

    ラベル、ボタンを追加。ボタン押下イベント追加

--- HelloSwift/ViewController.swift
+++ HelloSwift/ViewController.swift
@@ -10,9 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var toggleLabelText: Bool = false
+    var defaultLabelText: String? = ""
+    @IBOutlet weak var myLabel: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.defaultLabelText = self.myLabel.text
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +25,24 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func buttonPressed(_ sender: Any) {
+        var text: String?
+        if self.toggleLabelText == true {
+            text = self.defaultLabelText
+        }
+        else {
+            let locale = Locale(identifier: "ja_JP")
+            let fmt = DateFormatter()
+            fmt.locale = locale
+            fmt.timeStyle = .none
+            fmt.dateStyle = .long
+            
+            let now = Date()
+            text = fmt.string(from: now)
+        }
+        self.myLabel.text = text
+        self.toggleLabelText = !self.toggleLabelText
+    }
 
 }
 
